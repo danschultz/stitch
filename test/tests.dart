27d65878d4296509a962fb0bc54fcef4f8fae67b
Dan Schultz <schultz.t.dan@gmail.com>
2014-05-08 05:05:30 -0700

    Add tests for sprite sheet

--- test/tests.dart
+++ test/tests.dart
@@ -1,7 +1,9 @@
 library spritely.tests;
 
 import 'html_inspector_test.dart' as htmlInspectorTest;
+import 'sprite_sheet_test.dart' as spriteSheetTest;
 
 void main() {
   htmlInspectorTest.main();
+  spriteSheetTest.main();
 }
