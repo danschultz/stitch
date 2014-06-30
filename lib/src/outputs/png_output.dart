8cbd4dac6ee156c724c46eb64268fa30dda0e8ae
Dan Schultz <schultz.t.dan@gmail.com>
2014-06-30 00:41:19 -0700

    Disable alpha blending to preserve opacity of original sprites
    If the original sprite has transparency, alpha blending will change the level of
    transparency when it's combined into the sprite sheet. The original transparency
    needs to be preserved.
    
    Closes #14

--- lib/src/outputs/png_output.dart
+++ lib/src/outputs/png_output.dart
@@ -10,7 +10,7 @@ class PngOutput extends Output {
 
     var image = new Image(size.width, size.height);
     for (var sprite in sprites) {
-      copyInto(image, sprite.image, dstX: sprite.position.x, dstY: sprite.position.y);
+      copyInto(image, sprite.image, dstX: sprite.position.x, dstY: sprite.position.y, blend: false);
     }
 
     return new Asset.fromBytes(primaryInput.id.changeExtension(extension), encodePng(image));
